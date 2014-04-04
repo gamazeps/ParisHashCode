50af0fe61f4aa52ec4c8e57f7ddf9a3980b31c1b
Pierre-Hugues Husson <phh@phh.me>
2014-04-04 21:29:49 +0200

    First version

--- v1-fh.c
+++ v1-fh.c
@@ -3,12 +3,69 @@
 #include <sys/stat.h>
 #include <fcntl.h>
 #include <stdlib.h>
+#include <string.h>
+
+//Don't touch this
+char *values = NULL;
+
+typedef enum {
+	NOT_PAINTED,
+	PAINTED,
+	TO_DEPAINT
+} state_t;
+state_t *states = NULL;
+int w,h;
+int score_square(int x, int y, int size) {
+	int score = 0;
+	for(int i=x-size; i <= x+size; ++i) {
+		if(i < 0 || i >w)
+			return -1;
+		for(int j=y-size; j <= y+size; ++j) {
+			if(j<0 || j>h)
+				return -1;
+
+			volatile int pos = i + j*w;
+			if(states[pos] == NOT_PAINTED && values[pos])
+				score++;
+			if(!values[pos] &&  states[pos] != TO_DEPAINT)
+				score--;
+		}
+	}
+	return score;
+}
+
+void apply_square(int x, int y, int size) {
+	for(int i=x-size; i <= x+size; ++i) {
+		if(i < 0 || i >w) {
+			fprintf(stderr, "FAILED\n");
+			exit(1);
+		}
+		for(int j=y-size; j <= y+size; ++j) {
+			if(i < 0 || i >w) {
+				fprintf(stderr, "FAILED\n");
+				exit(1);
+			}
+			int pos = i + j*w;
+			if(states[pos] == NOT_PAINTED && values[pos])
+				states[pos]=PAINTED;
+			if(!values[pos] &&  states[pos] != TO_DEPAINT) {
+				states[pos] = TO_DEPAINT;
+			}
+		}
+	}
+	printf("PAINTSQ %d %d %d\n", x, y, size);
+}
 
 int main() {
 	FILE* fp = fopen("doodle.txt", "r");
-	int w,h;
-	fscanf(fp, "%d %d", &h, &w);
-	char *values = malloc(h*w);
+	fscanf(fp, "%d %d", &w, &h);
+
+	values = malloc(h*w);
+	bzero(values, w*h);
+
+	states = malloc(h*w*sizeof(state_t));
+	bzero(states, w*h);
+
 	for(int i=0; i < (w*h); ) {
 		char c;
 		fread(&c, 1, 1, fp);
@@ -18,5 +75,25 @@ int main() {
 		}
 		if(c == '.')
 			++i;
+	}
+	fclose(fp);
+
+	for(int size = 8; size; size--) {
+		int score_lim = size * size / 2;
+		for(int i=0; i < w; ++i) {
+			for(int j=0; j < h; ++j) {
+				int score = score_square(i, j, size);
+				if(score > score_lim) {
+					apply_square(i, j, size);
+				}
+			}
+		}
+	}
+	for(int i=0; i < w; ++i) {
+		for(int j=0; j < h; ++j) {
+			if(states[i+j*w] == TO_DEPAINT) {
+				printf("ERASECELL %d %d\n", i, j);
+			}
+		}
 	}
 }
