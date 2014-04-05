8d8102b97fae2eca4f277cfda2b096888312484e
Pierre-Hugues Husson <phh@phh.me>
2014-04-05 15:37:59 +0000

    bite

--- Samedi/v0.cpp
+++ Samedi/v0.cpp
@@ -108,11 +108,11 @@ float compute_coef_simp(int id, int dest1, int dest2, int rue) {
 	}
 	if(!voitures[id].temps_arrive) {
 		coef *= exp( (d1-d2 ) * 8000);
-	} else {
+	} /*else {
 		if( (d1) > 0.02) {
 	  		coef *= exp( (d1-d2 ) * 1000);
 		}
-	}
+	}*/
 	if(!rues[rue].parcourue)
 	  coef += (54000 - temps_restant) ;
 	return coef;
